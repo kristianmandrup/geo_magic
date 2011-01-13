cd999a31262bf03586807a17cc4d022c8b3ffa69
Kristian Mandrup <kmandrup@gmail.com>
2011-01-13 13:14:40 +0100

    added better dist calc specs

--- spec/geo_magic_calculate_spec.rb
+++ spec/geo_magic_calculate_spec.rb
@@ -10,17 +10,16 @@ describe "GeoMagic Calculate" do
     @lat2 = @lat1
   end
   
-  it "calculates distance using array args" do
+  it "calculates distance using array args - using algorithm haversine" do
     dist = GeoMagic::Calculate.distance [@long1, @lat1], [@long2, @lat2]    
     puts dist    
   end
 
-  it "Changing default distance algorithm" do
+  it "Changing default distance algorithm to vincenty" do
     GeoDistance.default_algorithm = :vincenty
     dist = GeoMagic::Calculate.distance [@long1, @lat1], [@long2, @lat2]    
     puts dist    
   end
-
 
   it "calculates distance using Point args" do
     from_point = GeoMagic::Point.new @long1, @lat1    
@@ -32,7 +31,7 @@ describe "GeoMagic Calculate" do
     puts dist    
   end
 
-  it "calculates distance using Location arg" do
+  it "calculates distance using Location arg from my location" do
     from_point = GeoMagic::Remote.my_location
     to_point = GeoMagic::Point.new @long2, @lat2
     
@@ -41,24 +40,24 @@ describe "GeoMagic Calculate" do
     dist = GeoMagic::Calculate.distance from_point, to_point
     puts dist    
   end
-  # 
-  # it "calculates distance using Hash args (short)" do
-  #   from_point = GeoMagic::Point.new(@long1, @lat1).to_hash :short
-  #   to_point = GeoMagic::Point.new(@long2, @lat2).to_hash :short
-  #   
-  #   puts "from: #{from_point}, to: #{to_point}"
-  #   
-  #   dist = GeoMagic::Calculate.distance from_point, to_point
-  #   puts dist    
-  # end
-  # 
-  # it "calculates distance using Hash args (long)" do
-  #   from_point = GeoMagic::Point.new(@long1, @lat1)
-  #   to_point = GeoMagic::Point.new(@long2, @lat2)
-  # 
-  #   puts "from: #{from_point}, to: #{to_point}"
-  #   
-  #   dist = GeoMagic::Calculate.distance from_point, to_point
-  #   puts dist    
-  # end
+  
+  it "calculates distance using Hash args (short)" do
+    from_point = GeoMagic::Point.new(@long1, @lat1).to_hash :short
+    to_point = GeoMagic::Point.new(@long2, @lat2).to_hash :short
+    
+    puts "from: #{from_point}, to: #{to_point}"
+    
+    dist = GeoMagic::Calculate.distance from_point, to_point
+    puts dist    
+  end
+  
+  it "calculates distance using Hash args (long)" do
+    from_point = GeoMagic::Point.new(@long1, @lat1)
+    to_point = GeoMagic::Point.new(@long2, @lat2)
+  
+    puts "from: #{from_point}, to: #{to_point}"
+    
+    dist = GeoMagic::Calculate.distance from_point, to_point
+    puts dist    
+  end
 end
