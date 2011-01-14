ee60ed30bb111aa1af96172bf1d2626d9e62d231
Kristian Mandrup <kmandrup@gmail.com>
2011-01-14 10:40:47 +0100

    improved creation of points

--- lib/geo_magic/map_point.rb
+++ lib/geo_magic/map_point.rb
@@ -8,9 +8,41 @@ module GeoMagic
       @latitude = latitude
       @longitude = longitude
     end  
+
+    def move_within distance
+      conversion = GeoDistance.radians_ratio(distance.unit)      
+
+      max_radius_rad = dist.distance
+      range = (max_radius_rad * normalize).to_i          
+
+      dlong = (get_random_radiant(range) / normalize)
+      dlat  = (get_random_radiant(range) / normalize)
+      
+      @latitude   += dlat
+      @longitude  += dlong
+    end
+
+    def move_distance radius
+      distance = radius.distance
+      conversion = GeoDistance.radians_ratio(distance.unit])      
+
+      max_radius_rad = distance.distance / conversion
+      range = (max_radius_rad * normalize).to_i     
+
+      dlong = (get_random_radiant(range) / normalize) * conversion
+      dlat  = (get_random_radiant(range) / normalize) * conversion
+      
+      GeoMagic::Point.new dist.latitude + dlat, dist.longitude + dlong      
+    end
     
     def within(distance) 
       GeoMagic::Radius.new self, distance
     end  
+    
+    protected
+    
+    def get_random_radiant(range)
+      GeoMagic::Radius.get_random_radiant(range)
+    end
   end
 end
--- lib/geo_magic/radius.rb
+++ lib/geo_magic/radius.rb
@@ -1,30 +1,33 @@
 module GeoMagic
   class Radius
     attr_accessor :center, :distance
-
-    RAD_PER_DEG = 0.017453293
-
-    # radius of the great circle in miles
-    # radius in kilometers...some algorithms use 6367
-    def rad
-      {:km => 6371, :miles => 3956, :feet => 20895592, :meters => 6371000}                     
-    end
     
     def initialize center, distance
       @center = center
       @distance = distance
     end  
+
+    def create_point_within 
+      conversion = GeoDistance.radians_ratio(distance.unit)      
+
+      max_radius_rad = dist.distance
+      range = (max_radius_rad * normalize).to_i          
+
+      dlong = (get_random_radiant(range) / normalize)
+      dlat  = (get_random_radiant(range) / normalize)
+      
+      GeoMagic::Point.new @center.latitude + dlat, @center.longitude + dlong
+    end
     
     def create_points number
-      conversion = (RAD_PER_DEG * rad[distance.unit])      
+      conversion = GeoDistance.radians_ratio(distance.unit)
 
-      max_radius_rad = distance.distance / conversion
-
-      range = (max_radius_rad * normalize).to_i     
+      max_radius_rad = distance.distance
+      range = (max_radius_rad * normalize).to_i
 
       number.times.inject([]) do |res, n|
-        dlong = (get_randowm_radiant(range) / normalize) * conversion
-        dlat  = (get_randowm_radiant(range) / normalize) * conversion
+        dlong = (get_random_radiant(range) / normalize)
+        dlat  = (get_random_radiant(range) / normalize)
 
         point = GeoMagic::Point.new @center.latitude + dlat, @center.longitude + dlong
         res << point
@@ -32,12 +35,16 @@ module GeoMagic
       end
     end  
 
-    def get_randowm_radiant range
-      rand(range) - (range / 2)
+    def get_random_radiant range
+      class.self.get_random_radiant range
+    end
+
+    def self.get_random_radiant range
+      rand(range * 2) - range
     end
     
     def normalize
-      1000000.0
+      100.0
     end
   end
 end
